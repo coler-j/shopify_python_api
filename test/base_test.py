e4cbf9c450bcaa60da51a582db1d792956134724
Coler <coler-j@users.noreply.github.com>
2021-05-04 22:19:35 -0400

    Rate limit api (#1)
    * wip
    
    * Update base_test.py

--- test/base_test.py
+++ test/base_test.py
@@ -1,3 +1,8 @@
+import pyactiveresource
+from pyactiveresource.connection import Response
+
+from shopify.base import ShopifyConnection
+
 import shopify
 from test.test_helper import TestCase
 from pyactiveresource.activeresource import ActiveResource
@@ -115,3 +120,66 @@ class BaseTest(TestCase):
         shopify.ShopifyResource.set_site(shop_url)
         res = shopify.Shop.current()
         self.assertEqual("Apple Computers", res.name)
+
+
+class ShopifyConnectionTest(TestCase):
+    @classmethod
+    def setUpClass(self):
+        shopify.ApiVersion.define_known_versions()
+        shopify.ApiVersion.define_version(shopify.Release("2019-04"))
+        self.session1 = shopify.Session("shop1.myshopify.com", "unstable", "token1")
+
+    @classmethod
+    def tearDownClass(self):
+        shopify.ApiVersion.clear_defined_versions()
+
+    def setUp(self):
+        super(ShopifyConnectionTest, self).setUp()
+
+    def tearDown(self):
+        shopify.ShopifyResource.clear_session()
+
+    def test_pyactiveresource_connection_open_is_called(self):
+        with patch("pyactiveresource.connection.Connection._open") as mock:
+            connection = ShopifyConnection(shopify.ShopifyResource.site)
+            connection._open()
+            mock.assert_called()
+
+    def test_response_is_returned_if_no_error_raised(self):
+        with patch("pyactiveresource.connection.Connection._open") as mock:
+            mock.return_value = "good response"
+            connection = ShopifyConnection(shopify.ShopifyResource.site)
+            self.assertEqual(connection._open(), "good response")
+
+    def test_connection_is_retried_if_429_raised(self):
+        with patch("pyactiveresource.connection.Connection._open") as mock:
+            error = pyactiveresource.connection.ConnectionError()
+            error.response.code = 429
+            error.response.headers = {"Retry-After": 1}
+            mock.side_effect = [error, error, "success"]
+
+            connection = ShopifyConnection(shopify.ShopifyResource.site)
+            connection._open()
+
+            # Should have been called twice with failure and one complete
+            self.assertEqual(len(mock.call_args_list), 3)
+
+    def test_non_connection_errors_are_raised_immediately(self):
+        with patch("pyactiveresource.connection.Connection._open") as mock:
+            mock.side_effect = Exception("Some error")
+
+            connection = ShopifyConnection(shopify.ShopifyResource.site)
+
+            with self.assertRaises(Exception):
+                connection._open()
+
+    def test_error_raised_immediately_if_non_429(self):
+        with patch("pyactiveresource.connection.Connection._open") as mock:
+            error = pyactiveresource.connection.ConnectionError()
+            error.response.code = 401
+            mock.side_effect = error
+
+            connection = ShopifyConnection(shopify.ShopifyResource.site)
+
+            with self.assertRaises(pyactiveresource.connection.ConnectionError):
+                connection._open()
